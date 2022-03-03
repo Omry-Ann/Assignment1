0ce148f3bdd237bd17b84f80aa66d63dd8c09940
Omri Yenon <omriyenon@Gevs-MacBook-Pro.local>
2022-03-03 16:57:11 +0200

    game-skeleton

---
+++ src/components/Game.vue
@@ -0,0 +1,51 @@
+<template>
+    <div >
+        <div class="game" >
+            <h3>Question Number: <br><br>{{currentQuestionIndex +1}}</h3>
+            <br>
+            {{currentQuestion}}
+            <input type="text">
+        </div>
+        <br><br>
+        <button @click="prev()">PREV</button>
+        <button @click="next()">NEXT</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            questions: [
+                {question: '1+2 =' , ans:'3'},
+                {question: '5+5 =' , ans:'10'}
+                ],
+            currentQuestionIndex: 0
+        };
+    },
+    computed: {
+        currentQuestion() {
+            return this.questions[this.currentQuestionIndex].question;
+        }
+    },
+    methods: {
+        prev() {
+            if (this.currentQuestionIndex > 0) 
+                this.currentQuestionIndex--;
+        },
+        next() {
+            if (this.currentQuestionIndex < this.questions.length-1) 
+                this.currentQuestionIndex++;
+        }
+    }
+}
+</script>
+
+<style>
+    .game{
+        width:300px;
+        height: 300px;
+        border:1px solid black;
+        margin: auto;
+    }
+</style>
--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="quiz">
     <topbar :fname="fname" :image="image" :currScore="currScore" :highScore="highScore" />
-    
-    <br><br><br><br>
-    
+    <br><br>
+    <game/>
+    <bottombar/>
   </div>
 </template>
 
 <script>
 import topbar from '../components/Topbar.vue'
+import bottombar from '../components/Bottombar.vue'
+import game from '../components/Game.vue'
 export default {
-  components: {topbar},
+  components: {topbar, bottombar, game},
   data(){
   return{
     currScore:0,
@@ -28,6 +30,10 @@ export default {
       else
         return require("../assets/icons/owl.png");
     }
+  
+  },
+  methods: {
+
   }
 }
 </script>
