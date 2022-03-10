7f2a62236e89ed2278ec67e396d3e23577bf9640
Omri Yenon <omriyenon@Gevs-MacBook-Pro.local>
2022-03-11 01:48:12 +0200

    some of the changes pr

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,39 +1,37 @@
 <template>
 
   <div>
-    <h1>{{msg}}</h1>
+    <h1>Register Here</h1>
     <hr size="3" noshade><br>
 
-    <label v-for="dataInput in dataInputs" :key="dataInput.name">
+    <label v-for="dataInput in dataInputs.slice(0,3)" :key="dataInput.name">
       <h2>{{dataInput.name}}</h2>
       <div v-if="dataInput.value===''">please enter your {{dataInput.name}} <br><br><input v-model="dataInput.value"></div>
-      <div v-else-if="validity(dataInput.name, dataInput.value)"> <input v-model="dataInput.value" class="Valid"></div>
-      <div v-else><p>{{dataInput.name}} is not valid</p> <input v-model="dataInput.value" class="Novalid"></div>
+      <div v-else-if="validity(dataInput.name, dataInput.value)"> <input v-model="dataInput.value" class="valid"></div>
+      <div v-else><p>{{dataInput.name}} is not valid</p> <input v-model="dataInput.value" class="no-valid"></div>
     </label>
    
     <h2>Gender:</h2>
-    <div v-if="gender!= ''">
-    <select v-model="gender" class="Valid">
-      <option disabled value=""></option>
-      <option value="Male">Male</option>
-      <option value="Female">Female</option>
-      <option value="Both">Both</option>
+    <div v-if="dataInputs[3].value!= ''">
+    <select v-model="dataInputs[3].value" class="valid">
+      <option value="M">Male</option>
+      <option value="F">Female</option>
+      <option value="B">Both</option>
     </select></div>
     <div v-else>gender has to be selected<br><br>
-    <select v-model="gender">
-      <option disabled value=""></option>
-      <option value="Male">Male</option>
-      <option value="Female">Female</option>
-      <option value="Both">Both</option>
+    <select v-model="dataInputs[3].value">
+      <option value="M">Male</option>
+      <option value="F">Female</option>
+      <option value="B">Both</option>
     </select></div>
 
     <h2>Your Avatar:</h2>
-    <label v-if="image ===''">please select an Avatar<br><br></label>
+    <label v-if="dataInputs[4].value ===''">please select an Avatar<br><br></label>
     <label v-for="img in imgs" :key="img.src">
-      <input type="radio" v-model="image" :value="img.value">
-      <img :src="img.src" alt="avatar" /></label><br><br>
+      <input type="radio" v-model="dataInputs[4].value" :value="img.value">
+      <img :src="img.src" :alt="img.value" /></label><br><br>
 
-    <button @click.prevent="submit"> Submit</button> <br><br> 
+    <button @click="submit"> Submit</button> <br><br> 
     <hr size="3" noshade>
   
   </div>
@@ -46,11 +44,10 @@ export default {
       dataInputs:[
         {name:"Full Name",value:''},
         {name:"Mail",value:''},
-        {name:"Phone Number",value:''}
+        {name:"Phone Number",value:''},
+        {name:"Gender",value:''},
+        {name:"Avatar",value:''}
       ],
-      gender: '',
-      image: '',
-      msg: 'Register Here',
       imgs:[
         {src: require("../assets/icons/dog.png") , value:"dog"},
         {src: require("../assets/icons/fish.png"), value:"fish"},
@@ -61,45 +58,36 @@ export default {
   methods: {
     validity: function(name,value){
       if(name === 'Full Name')
-        return value.match(/^([a-zA-Z]{1,} )([a-zA-Z]{1,})$/)? true :false;
+        return value.match(/^([a-zA-Z]{1,}\s)([a-zA-Z]{1,})$/)? true :false;
       else if(name === 'Mail')
         return value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)?  true : false;
-      else
+      else if(name === 'Phone Number')
         return value.match(/^\d{10}$/)? true : false;
+      else
+        return value != '';
     },
     submit: function(){
       let allvalid = this.dataInputs.reduce(
-        (acc,curr)=> acc && this.validity(curr.name, curr.value), true
-      ) && this.gender != '' && this.image != '';
+        (acc,curr)=> acc && this.validity(curr.name, curr.value), true);
       if(allvalid)
       {
         alert('submitted');
-        console.log(this.jsonOutput);
-        this.cleanForm()
+        this.dataInputs.forEach(input=>
+        console.log(input.name+":"+input.value));
+        this.dataInputs.forEach(input => {input.value=''});
       }
       else{
-        alert('please fill again');
+        alert('Some of the fields are invalid or incompleted');
       }
     },
-    cleanForm: function(){
-      this.dataInputs[0].value = '';
-      this.dataInputs[1].value = '';
-      this.dataInputs[2].value = '';
-      this.gender = '';
-      this.image = '';
-    }
+    // cleanForm: function(){
+    //   this.dataInputs.forEach(element => {
+    //     element.value=''
+    //   });
+    // }
   },
   computed: {
-    
-    jsonOutput: function(){
-      return [
-        ['fname', this.dataInputs[0].value],
-        ['mail', this.dataInputs[1].value],
-        ['phonenum', this.dataInputs[2].value],
-        ['gender', this.gender],
-        ['image', this.image]
-      ];
-    }
+   
   }
 }
 </script>
@@ -115,11 +103,11 @@ h1 {
   text-decoration-style:dotted;
 }
 
-.Valid {
+.valid {
   border-color:rgb(3, 245, 3);
 }
 
-.NoValid {
+.no-valid {
   border-color:red;
 }
 p{
